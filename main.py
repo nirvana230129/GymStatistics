05cc67f62715cb32696316532ec332e9aaee1906
arif_meilanov <arif.m.04@mail.ru>
2025-03-07 19:14:09 +0300

    description field was added

--- main.py
+++ main.py
@@ -16,26 +16,33 @@ class Database:
         self._cursor.execute(f'''
             CREATE TABLE IF NOT EXISTS {self.table_name} (
                 date DATE NOT NULL,
-                machine TEXT NOT NULL,
+                machine VARCHAR NOT NULL,
                 weight REAL,
                 rating INTEGER,
+                description TEXT,
                 PRIMARY KEY (date, machine)
             );
         ''')
 
-    def add_workout(self, date, machine, weight=None, rating=None):
+    def add_workout(self, date: datetime.date, machine: str, weight: float = None, rating: int = None,
+                    description: str = None):
         self._cursor.execute(f'''
-            INSERT INTO {self.table_name} (date, machine, weight, rating)
-            VALUES (?, ?, ?, ?)
-        ''', (date, machine, weight, rating))
+            INSERT INTO {self.table_name} (date, machine, weight, rating, description)
+            VALUES (?, ?, ?, ?, ?);
+        ''', (date, machine, weight, rating, description))
 
     def commit(self):
         self._connection.commit()
+
 
 db = Database('gym_tracker.db', 'workouts')
 db.drop_table()
 db.create_table()
 db.commit()
-db.add_workout(str(datetime.today().date()), 'бицепс',35)
-db.add_workout(datetime(2025, 3, 1).date(), 'Bench Press', 80, 4)
+db.add_workout(str(datetime.today().date()), 'бицепс', 35)
+db.add_workout(datetime(2025, 3, 1).date(), 'Bench Press', 60, 5)
+db.add_workout(datetime(2025, 3, 2).date(), 'Bench Press', 62.5, 4)
+db.add_workout(datetime(2025, 3, 3).date(), 'Bench Press', 65, 4)
+db.add_workout(datetime(2025, 3, 4).date(), 'Bench Press', 70, 2)
 db.commit()
+print(type(datetime(2025, 3, 4).date()))
