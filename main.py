7ccba2131dfc6d895369de5debd58a1727c3481e
arif_meilanov <arif.m.04@mail.ru>
2025-03-07 19:35:39 +0300

    try block for add_workout

--- main.py
+++ main.py
@@ -26,19 +26,22 @@ class Database:
 
     def add_workout(self, date: datetime.date, machine: str, weight: float = None, rating: int = None,
                     description: str = None):
-        self._cursor.execute(f'''
-            INSERT INTO {self.table_name} (date, machine, weight, rating, description)
-            VALUES (?, ?, ?, ?, ?);
-        ''', (date, machine, weight, rating, description))
+        try:
+            self._cursor.execute(f'''
+                INSERT INTO {self.table_name} (date, machine, weight, rating, description)
+                VALUES (?, ?, ?, ?, ?);
+            ''', (date, machine, weight, rating, description))
+        except sqlite3.IntegrityError:
+            raise Exception(f'Workout with {machine} on {date} already exists')
 
     def commit(self):
         self._connection.commit()
 
 
 db = Database('gym_tracker.db', 'workouts')
-db.drop_table()
-db.create_table()
-db.commit()
+# db.drop_table()
+# db.create_table()
+# db.commit()
 db.add_workout(str(datetime.today().date()), 'бицепс', 35)
 db.add_workout(datetime(2025, 3, 1).date(), 'Bench Press', 60, 5)
 db.add_workout(datetime(2025, 3, 2).date(), 'Bench Press', 62.5, 4)
