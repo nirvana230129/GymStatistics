941621e196b0f7ed2b7134b5e81b7a59aa6c8723
arif_meilanov <arif.m.04@mail.ru>
2025-03-07 17:10:06 +0300

    Database class

--- main.py
+++ main.py
@@ -3,15 +3,30 @@ import matplotlib.pyplot as plt
 from datetime import datetime
 
 
-conn = sqlite3.connect('gym_tracker.db')
-c = conn.cursor()
-c.execute('''
-          CREATE TABLE IF NOT EXISTS workouts (
-              date DATE,
-              machine TEXT,
-              weight REAL,
-              rating INTEGER,
-              PRIMARY KEY (date, machine)
-          )
-          ''')
-conn.commit()
+class Database:
+    def __init__(self, db_file, table_name):
+        self._connection = sqlite3.connect(db_file)
+        self._cursor = self._connection.cursor()
+        self.table_name = table_name
+
+    def drop_table(self):
+        self._cursor.execute(f'DROP TABLE IF EXISTS {self.table_name};')
+
+    def create_table(self):
+        self._cursor.execute(f'''
+            CREATE TABLE IF NOT EXISTS {self.table_name} (
+                date DATE NOT NULL,
+                machine TEXT NOT NULL,
+                weight REAL,
+                rating INTEGER,
+                PRIMARY KEY (date, machine)
+            );
+        ''')
+
+    def commit(self):
+        self._connection.commit()
+
+db = Database('gym_tracker.db', 'workouts')
+db.drop_table()
+db.create_table()
+db.commit()
