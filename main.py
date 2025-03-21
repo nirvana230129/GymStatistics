50a184cdddee460f710104290105848c6ef42de6
arif_meilanov <arif.m.04@mail.ru>
2025-03-21 23:53:36 +0300

    Interface for adding a workout has been added

--- main.py
+++ main.py
@@ -34,7 +34,7 @@ class Database:
     def commit(self):
         self._connection.commit()
 
-    def add_workout(self, date: datetime.date, exercise: str, weight: float = None, rating: int = None,
+    def add_workout(self, workout_date: datetime.date, exercise: str, weight: float = None, feeling_rating: int = None,
                     description: str = None):
         self._cursor.execute('SELECT id FROM Exercises WHERE name = ?;', (exercise,))
         data = self._cursor.fetchone()
@@ -46,9 +46,9 @@ class Database:
             self._cursor.execute(f'''
                 INSERT INTO Workouts (date, exercise_id, weight, rating, description)
                 VALUES (?, ?, ?, ?, ?);
-            ''', (date, exercise_id, weight, rating, description))
+            ''', (workout_date, exercise_id, weight, feeling_rating, description))
         except sqlite3.IntegrityError:
-            raise Exception(f'Workout with {exercise} on {date} already exists')
+            raise Exception(f'Workout with {exercise} on {workout_date} already exists')
 
     def add_exercise(self, exercise: str):
         try:
@@ -59,8 +59,23 @@ class Database:
         except sqlite3.IntegrityError:
             raise Exception(f'Exercise {exercise} already exists')
 
+    def find_exercise(self, exercise: str):
+        self._cursor.execute('SELECT id FROM Exercises WHERE name = ?;', (exercise,))
+        data = self._cursor.fetchone()
+        return bool(data)
+
+    def get_all_exercises(self):
+        self._cursor.execute('SELECT name FROM Exercises;')
+        data = self._cursor.fetchall()
+        return [i[0] for i in data]
+
+    def print_all(self):
+        self._cursor.execute('SELECT * FROM Workouts W JOIN Exercises E ON W.exercise_id = E.id')
+        data = self._cursor.fetchall()
+        print(data)
+
     def plot_weights(self, machine):
-        self._cursor.execute(f'''
+        self._cursor.execute('''
             SELECT date, weight 
             FROM Workouts 
             WHERE machine = ?
@@ -85,14 +100,106 @@ db.drop()
 db.create()
 db.commit()
 
-for exercise in ['подтягивания узкие', 'подтягивания широкие', 'ноги снизу вверх (первые 2)', 'дельта', 'спина',
+for i in ['подтягивания узкие', 'подтягивания широкие', 'ноги снизу вверх (первые 2)', 'дельта', 'спина',
                  'ноги сверху вниз (правый)', 'соку бачи вира', 'висюля', 'от сердца к солнцу', 'дорожка', 'бицепс']:
-    db.add_exercise(exercise)
+    db.add_exercise(i)
 db.commit()
-
 db.add_workout(str(datetime.today().date()), 'бицепс', 35)
-# db.add_workout(datetime(2025, 3, 1).date(), 'Bench Press', 60, 5)
-# db.add_workout(datetime(2025, 3, 2).date(), 'Bench Press', 62.5, 4)
-# db.add_workout(datetime(2025, 3, 3).date(), 'Bench Press', 65, 4)
-# db.add_workout(datetime(2025, 3, 4).date(), 'Bench Press', 70, 2)
 db.commit()
+db.print_all()
+
+
+def input_date():
+    def _input_date():
+        try:
+            if user_input == 't':
+                return datetime.now().date()
+            if 2 <= len(user_input):
+                for el in user_input:
+                    if not el.isdigit():
+                        sep = el
+                        month, day = map(int, user_input.split(sep))
+                        break
+                else:
+                    month, day = map(int, [user_input[0], user_input[1:]])
+                current_year = datetime.now().year
+                date = datetime(current_year, month, day)
+                if date > datetime.now():
+                    date = datetime(current_year - 1, month, day)
+                return date.date()
+        except ValueError:
+            print("\tIncorrect date format. Please enter the date in mm-dd format.")
+
+    res = None
+    while res is None:
+        user_input = input('\tEnter date (mm-dd) or "t" for today or "exit" to finish: ').strip().lower()
+        if user_input == 'exit':
+            break
+        res = _input_date()
+    return res
+
+def input_exercise():
+    def _input_exercise():
+        try:
+            if db.find_exercise(user_input):
+                return user_input
+
+            inp = ''
+            while inp not in ['y', 'n']:
+                inp = input(f'\tThere is no exercise "{user_input}" in the database. '
+                            f'Do you want to add it? (y/n): ').strip().lower()
+            if inp == 'y':
+                db.add_exercise(user_input)
+                return user_input
+        except ValueError:
+            print("\tIncorrect input. Please try again.")
+
+    res = None
+    while res is None:
+        user_input = input(f'\tEnter exercise ({", ".join(db.get_all_exercises())}): ').strip().lower()
+        if user_input == 'exit':
+            break
+        res = _input_exercise()
+    return res
+
+def input_num(field_name, dest_type: type):
+    res = None
+    while res is None:
+        user_input = input(f'\tEnter {field_name}: ').strip().lower()
+        if user_input == 'exit':
+            break
+        try:
+            res = dest_type(user_input)
+        except ValueError:
+            print(f"\tIncorrect input for type {dest_type}. Please try again.")
+    return res
+
+
+inp = input('Enter command: ')
+while inp != 'exit' or inp != '0':
+    if inp.lower() in ['add workout day', '1']:
+        date = input_date()
+        if date is not None:
+            print('-' * 60)
+
+            exercise = input_exercise()
+            if exercise is not None:
+                print('-' * 60)
+
+                weight = input_num('weight', float)
+                if weight is not None:
+                    print('-' * 60)
+
+                    rating = input_num('rating', int)
+                    if rating is not None:
+                        print('-' * 60)
+
+                        description = input('    Enter description: ')
+
+                        db.add_workout(date, exercise, weight, rating, description)
+                        # db.add_workout(str(datetime.today().date()), 'висюля', weight, rating, description)
+
+    elif inp.lower() in ['Print all', '2']:
+        db.print_all()
+
+    inp = input('Enter command: ')
