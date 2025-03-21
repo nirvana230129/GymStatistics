9d3690ffd3801f3aa111182dd1c97eb8fd6316ad
arif_meilanov <arif.m.04@mail.ru>
2025-03-22 00:31:18 +0300

    Interface was wrapped into class Interface

--- main.py
+++ main.py
@@ -50,6 +50,12 @@ class Database:
         except sqlite3.IntegrityError:
             raise Exception(f'Workout with {exercise} on {workout_date} already exists')
 
+    def find_workout(self, workout_date: datetime.date, exercise_id: int):
+        self._cursor.execute('SELECT * FROM Workouts WHERE date = ? AND exercise_id = ?;',
+                             (workout_date, exercise_id))
+        data = self._cursor.fetchone()
+        return data[0] if data else None
+
     def add_exercise(self, exercise: str):
         try:
             self._cursor.execute('''
@@ -62,7 +68,7 @@ class Database:
     def find_exercise(self, exercise: str):
         self._cursor.execute('SELECT id FROM Exercises WHERE name = ?;', (exercise,))
         data = self._cursor.fetchone()
-        return bool(data)
+        return data[0] if data else None
 
     def get_all_exercises(self):
         self._cursor.execute('SELECT name FROM Exercises;')
@@ -94,112 +100,129 @@ class Database:
         plt.show()
 
 
-db = Database('gym_tracker.db')
+class Interface:
+    def __init__(self, db_file, clear=False):
+        self.db = Database('gym_tracker.db')
+        if clear:
+            self.db.drop()
+        self.db.create()
+        self.db.commit()
 
-db.drop()
-db.create()
-db.commit()
+    def fill_rubbish(self):
+        for i in ['подтягивания узкие', 'подтягивания широкие', 'ноги снизу вверх (первые 2)', 'дельта', 'спина',
+                  'ноги сверху вниз (правый)', 'соку бачи вира', 'висюля', 'от сердца к солнцу', 'дорожка', 'бицепс']:
+            self.db.add_exercise(i)
+        self.db.commit()
+        self.db.add_workout(str(datetime.today().date()), 'бицепс', 35)
+        self.db.commit()
 
-for i in ['подтягивания узкие', 'подтягивания широкие', 'ноги снизу вверх (первые 2)', 'дельта', 'спина',
-                 'ноги сверху вниз (правый)', 'соку бачи вира', 'висюля', 'от сердца к солнцу', 'дорожка', 'бицепс']:
-    db.add_exercise(i)
-db.commit()
-db.add_workout(str(datetime.today().date()), 'бицепс', 35)
-db.commit()
-db.print_all()
-
-
-def input_date():
+    @staticmethod
     def _input_date():
-        try:
-            if user_input == 't':
-                return datetime.now().date()
-            if 2 <= len(user_input):
-                for el in user_input:
-                    if not el.isdigit():
-                        sep = el
-                        month, day = map(int, user_input.split(sep))
-                        break
-                else:
-                    month, day = map(int, [user_input[0], user_input[1:]])
-                current_year = datetime.now().year
-                date = datetime(current_year, month, day)
-                if date > datetime.now():
-                    date = datetime(current_year - 1, month, day)
-                return date.date()
-        except ValueError:
-            print("\tIncorrect date format. Please enter the date in mm-dd format.")
+        def _input_date():
+            try:
+                if user_input == 't':
+                    return datetime.now().date()
+                if 2 <= len(user_input):
+                    for el in user_input:
+                        if not el.isdigit():
+                            sep = el
+                            month, day = map(int, user_input.split(sep))
+                            break
+                    else:
+                        month, day = map(int, [user_input[0], user_input[1:]])
+                    current_year = datetime.now().year
+                    date = datetime(current_year, month, day)
+                    if date > datetime.now():
+                        date = datetime(current_year - 1, month, day)
+                    return date.date()
+            except ValueError:
+                print("\tIncorrect date format. Please enter the date in mm-dd format.")
 
-    res = None
-    while res is None:
-        user_input = input('\tEnter date (mm-dd) or "t" for today or "exit" to finish: ').strip().lower()
-        if user_input == 'exit':
-            break
-        res = _input_date()
-    return res
+        res = None
+        while res is None:
+            user_input = input('\tEnter date (mm-dd) or "t" for today or "exit" to finish: ').strip().lower()
+            if user_input == 'exit':
+                break
+            res = _input_date()
+        return res
 
-def input_exercise():
-    def _input_exercise():
-        try:
-            if db.find_exercise(user_input):
-                return user_input
+    @staticmethod
+    def _input_num(field_name, dest_type: type):
+        res = None
+        while res is None:
+            user_input = input(f'\tEnter {field_name}: ').strip().lower()
+            if user_input == 'exit':
+                break
+            try:
+                res = dest_type(user_input)
+            except ValueError:
+                print(f"\tIncorrect input for type {dest_type}. Please try again.")
+        return res
 
-            inp = ''
-            while inp not in ['y', 'n']:
-                inp = input(f'\tThere is no exercise "{user_input}" in the database. '
-                            f'Do you want to add it? (y/n): ').strip().lower()
-            if inp == 'y':
-                db.add_exercise(user_input)
-                return user_input
-        except ValueError:
-            print("\tIncorrect input. Please try again.")
+    def _input_exercise(self):
+        def _input_exercise():
+            try:
+                if self.db.find_exercise(user_input):
+                    return user_input
 
-    res = None
-    while res is None:
-        user_input = input(f'\tEnter exercise ({", ".join(db.get_all_exercises())}): ').strip().lower()
-        if user_input == 'exit':
-            break
-        res = _input_exercise()
-    return res
+                inp = ''
+                while inp not in ['y', 'n']:
+                    inp = input(f'\tThere is no exercise "{user_input}" in the database. '
+                                f'Do you want to add it? (y/n): ').strip().lower()
+                if inp == 'y':
+                    self.db.add_exercise(user_input)
+                    return user_input
+            except ValueError:
+                print("\tIncorrect input. Please try again.")
 
-def input_num(field_name, dest_type: type):
-    res = None
-    while res is None:
-        user_input = input(f'\tEnter {field_name}: ').strip().lower()
-        if user_input == 'exit':
-            break
-        try:
-            res = dest_type(user_input)
-        except ValueError:
-            print(f"\tIncorrect input for type {dest_type}. Please try again.")
-    return res
+        res = None
+        while res is None:
+            user_input = input(f'\tEnter exercise ({", ".join(self.db.get_all_exercises())}): ').strip().lower()
+            if user_input == 'exit':
+                break
+            res = _input_exercise()
+        return res
 
+    def add_workout(self):
+        date = self._input_date()
+        if date is None:
+            return None
+        print('-' * 60)
+
+        exercise = self._input_exercise()
+        if exercise is None:
+            return None
+        print('-' * 60)
+
+        weight = self._input_num('weight', float)
+        if weight is None:
+            return None
+        print('-' * 60)
+
+        rating = self._input_num('rating', int)
+        if rating is None:
+            return None
+        print('-' * 60)
+
+        description = input('    Enter description: ')
+
+        self.db.add_workout(date, exercise, weight, rating, description)
+        self.db.commit()
+        return self.db.find_workout(date, self.db.find_exercise(exercise))
+
+    def print_all(self):
+        self.db.print_all()
+
+
+interface = Interface(db_file='gym_tracker.db', clear=True)
+interface.fill_rubbish()
 
 inp = input('Enter command: ')
 while inp != 'exit' or inp != '0':
     if inp.lower() in ['add workout day', '1']:
-        date = input_date()
-        if date is not None:
-            print('-' * 60)
-
-            exercise = input_exercise()
-            if exercise is not None:
-                print('-' * 60)
-
-                weight = input_num('weight', float)
-                if weight is not None:
-                    print('-' * 60)
-
-                    rating = input_num('rating', int)
-                    if rating is not None:
-                        print('-' * 60)
-
-                        description = input('    Enter description: ')
-
-                        db.add_workout(date, exercise, weight, rating, description)
-                        # db.add_workout(str(datetime.today().date()), 'висюля', weight, rating, description)
+        interface.add_workout()
 
     elif inp.lower() in ['Print all', '2']:
-        db.print_all()
+        interface.print_all()
 
     inp = input('Enter command: ')
